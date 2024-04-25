cad64a5cb3581857a17faedf1f6a5cf05c18422b
jrazad10 <jrazad10@gmail.com>
2024-04-25 23:34:41 +0600

    banner section done of homepage

---
+++ src/components/Banner.jsx
@@ -0,0 +1,40 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/gwzZtCJ/paint-draw-banner-template-23-2148516518.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/grVjTgV/images.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/8bb46dz/Deidre-Adams-Adams-Tracings13-detail1-feature.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/LRwq0kB/images.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,21 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
+
+    const links = <>
+        <NavLink><a>Item 1</a></NavLink>
+        <NavLink>
+            <a>Parent</a>
+            <ul className="p-2">
+                <li> <a>Submenu 1</a></li>
+                <li> <a>Submenu 2</a></li>
+                {/* <li><a>Submenu 2</a></li> */}
+            </ul>
+        </NavLink>
+        <NavLink><a>Item 3</a></NavLink>
+    </>
+
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -10,7 +25,12 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+
+
+                            {links}
+
+
+                            {/* <li><a>Item 1</a></li>
                             <li>
                                 <a>Parent</a>
                                 <ul className="p-2">
@@ -18,14 +38,18 @@ const Navbar = () => {
                                     <li><a>Submenu 2</a></li>
                                 </ul>
                             </li>
-                            <li><a>Item 3</a></li>
+                            <li><a>Item 3</a></li> */}
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-xl">Yousuf's A&C</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
+
+
+
+
+                        {/* <li><a>Item 1</a></li>
                         <li>
                             <details>
                                 <summary>Parent</summary>
@@ -35,7 +59,7 @@ const Navbar = () => {
                                 </ul>
                             </details>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Item 3</a></li> */}
                     </ul>
                 </div>
                 <div className="navbar-end">
