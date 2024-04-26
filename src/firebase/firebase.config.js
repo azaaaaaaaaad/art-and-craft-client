0514eded89942ce798f71c5539957222feab92de
jrazad10 <jrazad10@gmail.com>
2024-04-26 20:17:37 +0600

    updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDvaeU2wXS0PdsOQeKMRCqs2RdHGbC0it4",
-  authDomain: "yousuf-s-art-and-craft.firebaseapp.com",
-  projectId: "yousuf-s-art-and-craft",
-  storageBucket: "yousuf-s-art-and-craft.appspot.com",
-  messagingSenderId: "133519069606",
-  appId: "1:133519069606:web:6899dc1a3e05034d303f67"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
